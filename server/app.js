26eaced20c3f414aed747b68e7134de6d8c7a095
Kevin Phan <kevin@insurecert.com>
2022-07-06 20:04:06 -0700

    setup client and server for react/node expressjs app

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const db = require("./db");
+
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
